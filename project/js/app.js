0414d580251dd6cc5e76ce2da167b7c879db1dd2
Kessy Sabrinagit config --global user.name Kessy <kessy.oliveira@novaandradina.org>
2020-12-22 16:35:36 -0300

    final

--- project/js/app.js
+++ project/js/app.js
@@ -1,66 +1,71 @@
+
 const showMenu = (toggleId, navId) => {
-    const toggle = document.querySelector(toggleId),
-      nav = document.querySelector(navId)
-  
-    if (toggle && nav) {
-      toggle.addEventListener("click", () => {
-        nav.classList.toggle("show")
-      })
-    }
-  }
-  showMenu('#toggleId', '#naveg')
-  
-  /*===== REMOVE MENU =====*/
-  const navLink = document.querySelectorAll('.nav__link'),
-    navMenu = document.querySelector('#naveg')
-  
-  function linkAction() {
-    navMenu.classList.remove('show')
-  }
-  
-  navLink.forEach(n => n.addEventListener('click', linkAction))
+  const toggle = document.querySelector(toggleId),
+    nav = document.querySelector(navId)
 
-  const BASE_URL ='http://localhost:1337/';
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show")
+    })
+  }
+}
+showMenu('#toggleId', '#naveg')
 
-  async function carregarDados(colecao){
-        const resposta = await fetch(BASE_URL + colecao);
-        const dados = await resposta.json();
-        return dados;
-  }
-  async function buscarMotorcycles(){
-      const motos = carregarDados('/motorcycles');
-      return motos;
-  }
-  async function buscarMaterials(){
-      const materiais = await carregarDados('/materials');
-      return materiais;
-  }
-  async function buscarMotoPorType(tipo){
-        const motos = await carregarDados('/motorcycles?type=${tipo}');
-        return motos[0].name;
-    }
-async function incluirMotosProntasnoDOM(){
-    const motos = await buscarMotoPorType('pronto'),
-        container = document.querySelector("#modelosProntos");
-    for(const moto of motos){
-        const {price, image} = moto,
-        preco = price.toLocaleString("pt-br", {style: "currency", currency: "BRL"});
-        container.innerHTML += `
+/*===== REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link'),
+  navMenu = document.querySelector('#naveg')
+
+function linkAction() {
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const BASE_URL = 'http://localhost:1337';
+
+async function carregarDados(colecao) {
+  const resposta = await fetch(BASE_URL + colecao);
+  const dados = await resposta.json();
+  return dados;
+}
+async function buscarMotorcycles() {
+  const motos = carregarDados('/requests');
+  return motos;
+}
+async function buscarMaterials() {
+  const materiais = await carregarDados('/materials');
+  return materiais;
+}
+async function buscarMotoPorType(tipo) {
+  const motos = await carregarDados(`/requests?type=${tipo}`);
+  return motos;
+}
+async function incluirMotosProntasnoDOM() {
+  const motos = await buscarMotoPorType('pronto'),
+    container = document.querySelector("#modelosProntos");
+  for (const moto of motos) {
+    const { price, photo, name, description } = moto;
+    console.log(price);
+      preco = price.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+    container.innerHTML += `
             <div class="moto">
-                <img src=${BASE_URL}${image.url} id="moto">
-                <p class="preco"> ${preco}</p>
+                <img src=${BASE_URL}${photo.url} id="moto">
+                <div class="info">
+                  <p class="preco"> ${preco}</p>  <hr/>
+                  <p class="preco">${name}</p> <hr/>
+                </div>
             </div>
             `
-            ;
-        
-    }
+      ;
+
+  }
 
 }
-    
+incluirMotosProntasnoDOM();
 
 
 
 
 
-    
-    
+
+
